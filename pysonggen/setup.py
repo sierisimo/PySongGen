f855a6e0a519dd8c4cb0451d0138b0eb6944aab1
Sier Violencia <sierisimo@gmail.com>
2014-11-18 20:53:42 -0600

    setup.py added

---
+++ pysonggen/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = "PySongGen",
+    version      = "0.0.1",
+    py_modules   = ["pysonggen"],
+    author       = "Sier Violencia",
+    author_email = "sierisimo@gmail.com",
+    url          = "http://github.com/sierisimo/PySongGen",
+    description  = "A Python3 module that let's you make a melody with a grammar file"
+)
